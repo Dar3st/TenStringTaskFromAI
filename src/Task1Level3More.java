120aca8c90130fa916d4878713b376e92548d7e7
dar3st <d2rk3st@gmail.com>
2025-10-04 12:52:31 +0300

    ...

--- src/Task1Level3More.java
+++ src/Task1Level3More.java
@@ -7,8 +7,18 @@ public class Task1Level3More {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
         String[] input = {"9806567890", "89990980644", "+79005556134", "89160164366"};
+        formatedPhoneNumber(input);
+        s.close();
+    }
 
-        for(String number : input) {
+
+
+    /**
+     * Метод преобразующий пользовательский ввод номера
+     * @param numbers - исходный массив данных(список номеров)
+     */
+    public static void formatedPhoneNumber(String[] numbers){
+        for(String number : numbers) {
             String digits = number;
             if (digits.startsWith("+7")) {
                 digits = digits.substring(2);
@@ -22,13 +32,17 @@ public class Task1Level3More {
                     digits.substring(8);
 
             System.out.println(number + " -> " + formatted);
-            isValidPhoneNumber(formatted);
-            System.out.println("---"); // разделитель для читаемости
-        }
-        s.close();
-    }
 
-    public static void isValidPhoneNumber(String number) {
+            isValidPhoneNumber(formatted);
+
+            System.out.println("---");
+        }
+    }
+    /**
+     * Метод проверяет номер на соответствие формата и выводит информацию по номеру
+     * @param number
+     */
+    private static void isValidPhoneNumber(String number) {
         // ИСПРАВЛЕННОЕ регулярное выражение для формата +7-XXX-XXX-XX-XX
         String baseRegex = "\\+7-(\\d{3})-(\\d{3})-(\\d{2})-(\\d{2})";
 
@@ -36,10 +50,10 @@ public class Task1Level3More {
         Matcher matcher = pattern.matcher(number);
 
         if (matcher.matches()) {
-            System.out.println("Код оператора: " + "\t\t\t" + matcher.group(1)); // Первые 3 цифры после +7-
-            System.out.println("Первая часть номера: " + "\t" + matcher.group(2)); // Следующие 3 цифры
-            System.out.println("Вторая часть номера: " + "\t" + matcher.group(3)); // Следующие 2 цифры
-            System.out.println("Третья часть номера: " + "\t" + matcher.group(4)); // Последние 2 цифры
+            System.out.println("Код оператора: " + "\t\t\t" + matcher.group(1));
+            System.out.println("Первая часть номера: " + "\t" + matcher.group(2));
+            System.out.println("Вторая часть номера: " + "\t" + matcher.group(3));
+            System.out.println("Третья часть номера: " + "\t" + matcher.group(4));
             System.out.println("✓ Номер телефона корректен");
         } else {
             System.out.println("✗ Неверный формат номера!");
